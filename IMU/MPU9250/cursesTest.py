d3466047754ce58c987fa1ec186597dcd35a3910
Ayberk Yaraneri <ayberkyaraneri@gmail.com>
2019-02-25 17:55:34 -0600

    .

--- IMU/MPU9250/cursesTest.py
+++ IMU/MPU9250/cursesTest.py
@@ -13,11 +13,15 @@ def testIMU(stdscr):
     curses.init_pair(1, curses.COLOR_RED, curses.COLOR_WHITE)
     curses.init_pair(2, curses.COLOR_BLUE, curses.COLOR_WHITE)
     curses.init_pair(3, curses.COLOR_GREEN, curses.COLOR_WHITE)
-    curses.init_pair(4, curses.COLOR_PINK, curses.COLOR_WHITE)
+    color = 1
 
     while True:
         gyro = IMU1.readGyro()
         heading = IMU1.curHeading()
+
+        color += 1
+        if color == 4:
+            color = 1
 
         stdscr.erase()
         stdscr.addstr(0,0,"GX: " + str(gyro['x']))
@@ -25,10 +29,10 @@ def testIMU(stdscr):
         stdscr.addstr(2,0,"GZ: " + str(gyro['z']))
         stdscr.addstr(3,0,"ROLL: " + str(heading['roll']))
         stdscr.addstr(4,0,"PITCH: " + str(heading['pitch']))
-        stdscr.addstr(5,0,"YAW: " + str(heading['yaw']), curses.color_pair(1))
+        stdscr.addstr(5,0,"YAW: " + str(heading['yaw']), curses.color_pair(color))
         stdscr.addstr(3,50,"geoff gay")
         stdscr.refresh()
-        time.sleep(0.1)
+        time.sleep(0.3)
 
 def main():
     curses.wrapper(testIMU)
