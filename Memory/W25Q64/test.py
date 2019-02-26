37180f723749a312e03061ff79049e450e244062
Ayberk Yaraneri <ayberkyaraneri@gmail.com>
2019-02-26 16:44:05 -0600

    Update test.py

--- Memory/W25Q64/test.py
+++ Memory/W25Q64/test.py
@@ -8,8 +8,9 @@ spi = spidev.SpiDev()
 spi.max_speed_hz = 500000
 spi.mode = 0
 spi.open(0, spi_channel)
-
+print("SPI Channel Initialized.")
+time.sleep(1)
 print("sending data now...")
 time.sleep(0.5)
-data = spi.xfer2(0x90, 0x00])
+data = spi.xfer2([0x90, 0x00])
 print(data)
