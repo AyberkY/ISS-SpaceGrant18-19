3d5f397a87438af5aa10ff6fdbae9a5ac8a3b4bd
Ayberk Yaraneri <ayberkyaraneri@gmail.com>
2019-02-26 16:18:22 -0600

    Update test.py

--- Memory/W25Q64/test.py
+++ Memory/W25Q64/test.py
@@ -1,1 +1,15 @@
-print('test')
+#Test code for W25Q64FV SPI Flash chipself.
+#02/26/2019
+import spidev
+import time
+
+spi_channel = 0
+spi = spidev.SpiDev()
+spi.max_speed_hz = 500000
+spi.mode = 0
+spi.open(0, spi_channel)
+
+print("sending data now...")
+time.sleep(0.5)
+data = spi.xfer2(0x90, 0x00])
+print(data)
