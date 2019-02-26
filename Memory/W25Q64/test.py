6c408cab764c6b2778f43dac659bab8447033562
Ayberk Yaraneri <ayberkyaraneri@gmail.com>
2019-02-26 16:50:28 -0600

    Update test.py

--- Memory/W25Q64/test.py
+++ Memory/W25Q64/test.py
@@ -5,7 +5,7 @@ import time
 
 spi_channel = 0
 spi = spidev.SpiDev()
-spi.max_speed_hz = 500000
+#spi.max_speed_hz = 500000
 spi.mode = 0
 spi.open(0, spi_channel)
 print("SPI Channel Initialized.")
