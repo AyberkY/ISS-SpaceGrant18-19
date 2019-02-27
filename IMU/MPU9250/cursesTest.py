964992750420c6e0ac60800306fea76e47fa5445
Misapo <47199617+Misapo@users.noreply.github.com>
2019-02-26 20:17:25 -0600

    i

--- IMU/MPU9250/cursesTest.py
+++ IMU/MPU9250/cursesTest.py
@@ -32,7 +32,7 @@ def testIMU(stdscr):
         stdscr.addstr(3,50,"geoff gay", curses.color_pair(color))
         stdscr.addstr(6,0,'TIME ELAPSED: ' + str(IMU1.timeElapsed(time.time())))
         stdscr.refresh()
-        time.sleep(0.5)
+        time.sleep(0.01)
 
 def main():
     curses.wrapper(testIMU)
