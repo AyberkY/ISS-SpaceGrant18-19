829fa0eaa7a496eadb5496ace4c06cf2e136c316
ayberky <ayberkyaraneri@gmail.com>
2019-03-17 21:18:27 -0500

    .

--- GPS/get_gps_data.py
+++ GPS/get_gps_data.py
@@ -5,6 +5,7 @@ port = '/dev/ttyS0'                       #defines the serial port connected to
 ser = serial.Serial(port, baudrate = 9600, timeout = 0.5)    #creates a serial object
 
 def searchDATA(data):
+    dataOUT = ''
     if data.find('GGA') > 0:                #searches for $GPGGA in the GPS information
         dataGPS = pynmea2.parse(data)       #library does this stuff somehow
         dataOUT = [dataGPS.timestamp, dataGPS.lat + ' ' + dataGPS.lat_dir, dataGPS.lon + ' ' + dataGPS.lon_dir, dataGPS.altitude + ' ' + dataGPS.altitude_units]
