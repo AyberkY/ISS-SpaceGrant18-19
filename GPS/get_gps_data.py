b0338696a32439b33449d705f6d92e695d275d4f
ayberky <ayberkyaraneri@gmail.com>
2019-03-17 21:21:04 -0500

    .

--- GPS/get_gps_data.py
+++ GPS/get_gps_data.py
@@ -8,7 +8,7 @@ def searchDATA(data):
     dataOUT = ''
     if data.find('GGA') > 0:                #searches for $GPGGA in the GPS information
         dataGPS = pynmea2.parse(data)       #library does this stuff somehow
-        dataOUT = [dataGPS.timestamp, dataGPS.lat + ' ' + dataGPS.lat_dir, dataGPS.lon + ' ' + dataGPS.lon_dir, dataGPS.altitude + ' ' + dataGPS.altitude_units]
+        dataOUT = [str(dataGPS.timestamp), str(dataGPS.lat) + ' ' + str(dataGPS.lat_dir), str(dataGPS.lon) + ' ' + str(dataGPS.lon_dir), str(dataGPS.altitude) + ' ' + str(dataGPS.altitude_units)]
     print( dataOUT )
     return dataOUT
 
