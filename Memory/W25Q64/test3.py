a4818269ec17b32205c383012e78a3bb97b420aa
Kenneth Tochihara <ktt3@illinois.edu>
2019-03-20 16:49:07 -0500

    Update test3.py

--- Memory/W25Q64/test3.py
+++ Memory/W25Q64/test3.py
@@ -2,3 +2,12 @@ import spidev
 
 spi = spidev.SpiDev()
 spi.open(0, 0)
+
+#Raspi 3 Pinout Configuration
+CLK = 23
+MISO = 21
+MOSI = 19
+CS = 24
+
+#SPI Module Function Dictionaries
+moduleFunctions = {}
