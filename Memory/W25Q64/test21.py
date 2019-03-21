5b5e85e4654c58bf08b9ddd8b773f4e08a03f257
Kenneth Tochihara <ktt3@illinois.edu>
2019-03-21 02:31:18 -0500

    Update test21.py

--- Memory/W25Q64/test21.py
+++ Memory/W25Q64/test21.py
@@ -119,10 +119,10 @@ chip = spiflash(bus = 0, cs = 0)
 try:
     print("Reading Data")
     while True:
-        block  = int(hex(input("Block (0-127): ")), 16)
-        sector = hex(input("Sector (0-16): "))
-        page   = hex(input("Page   (0-16): "))
-        sectPg = int((sector[2:]+pageN[2:]), 16)
+        block  = int(input("Block (00-FF): "), 16)
+        sector = input("Sector (0-F): ")
+        page   = input("Page   (0-F): ")
+        sectPg = int(sector+pageN, 16)
 
         data = chip.read_page(block,  sectPage)
         chip.print_page(data)
