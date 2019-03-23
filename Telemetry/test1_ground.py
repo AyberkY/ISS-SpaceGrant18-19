99640bd081cf536c7f3ae7b2d394b9a521b1dd0c
mayachatt <44557298+mayachatt@users.noreply.github.com>
2019-03-22 23:39:04 -0500

    splitting into air and ground code

--- Telemetry/test1_ground.py
+++ Telemetry/test1_ground.py
@@ -42,27 +42,3 @@ print('Sent Hello World message!')
 # rate, in fact it can only receive and process one 252 byte packet at a time.
 # This means you should only use this for low bandwidth scenarios, like sending
 # and receiving a single message at a time.
-print('Waiting for packets...')
-
-while True:
-    packet = rfm9x.receive()
-    # Optionally change the receive timeout from its default of 0.5 seconds:
-    #packet = rfm9x.receive(timeout=5.0)
-    # If no packet was received during the timeout then None is returned.
-    if packet is None:
-        # Packet has not been received
-        print('Received nothing! Listening again...')
-    else:
-        # Received a packet!
-        # Print out the raw bytes of the packet:
-        print('Received (raw bytes): {0}'.format(packet))
-        # And decode to ASCII text and print it too.  Note that you always
-        # receive raw bytes and need to convert to a text format like ASCII
-        # if you intend to do string processing on your data.  Make sure the
-        # sending side is sending ASCII data before you try to decode!
-        packet_text = str(packet, 'ascii')
-        print('Received (ASCII): {0}'.format(packet_text))
-        # Also read the RSSI (signal strength) of the last received message and
-        # print it.
-        rssi = rfm9x.rssi
-        print('Received signal strength: {0} dB'.format(rssi))
