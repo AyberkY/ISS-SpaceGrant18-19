83ef0674f53b0c90393def6c68e1002773a3e43b
davidcrobbins <47231078+davidcrobbins@users.noreply.github.com>
2019-03-29 16:19:35 -0500

    .....

--- GPS/crow_flies.py
+++ GPS/crow_flies.py
@@ -30,7 +30,7 @@ def convert(list):
 
     return LAT,LON
 
-def findDISTANCE(initLAT=40.11, initLON=-88.238, LAT, LON, R=6371*10**3):     #defines a function that takes an initial lon and an initial lat R=radius of earth
+def findDISTANCE(LAT, LON, R=6371*10**3, initLAT=40.11, initLON=-88.238):     #defines a function that takes an initial lon and an initial lat R=radius of earth
     deltaLAT = LAT - initLAT
     deltaLON = LON - initLON
 
