b62d9973db3899dc892a0151ce0b1f0020a4959e
mayachatt <44557298+mayachatt@users.noreply.github.com>
2019-03-29 16:17:57 -0500

    slower sleep

--- IMU/MPU9250/cursesTest.py
+++ IMU/MPU9250/cursesTest.py
@@ -39,7 +39,7 @@ def testIMU(stdscr):
         stdscr.addstr(6,0,'TIME ELAPSED: ' + str(IMU1.timeElapsed(time.time())))
         stdscr.refresh()
 #<<<<<<< HEAD
-        time.sleep(0.01)
+        time.sleep(1000)
 #=======
 #        time.sleep(0.1)
 #>>>>>>> 596fa30b72b57fc483e3da8d0fff764c917f38d8
