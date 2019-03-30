e51aa276548022b672e75e46eb4c6db5fe4805d8
Ayberk Yaraneri <35669067+AyberkY@users.noreply.github.com>
2019-03-30 03:12:39 -0500

    Create LEDtest2.py

---
+++ Test/LEDtest2.py
@@ -0,0 +1,35 @@
+"""
+LED Test code!
+Pin assignments:
+GPIO6 = ORANGE
+GPIO13 = GREEN
+GPIO19 = BLUE
+"""
+
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(6, GPIO.OUT)
+GPIO.setup(13, GPIO.OUT)
+GPIO.setup(19, GPIO.OUT)
+
+while True:
+    GPIO.output(6, GPIO.HIGH)
+    GPIO.output(13, GPIO.LOW)
+    GPIO.output(19, GPIO.LOW)
+
+    time.sleep(0.25)
+
+    GPIO.output(6, GPIO.LOW)
+    GPIO.output(13, GPIO.HIGH)
+    GPIO.output(19, GPIO.LOW)
+
+    time.sleep(0.25)
+
+    GPIO.output(6, GPIO.LOW)
+    GPIO.output(13, GPIO.LOW)
+    GPIO.output(19, GPIO.HIGH)
+
+    time.sleep(0.25)
