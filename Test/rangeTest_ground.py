bd5dd79d46a635be85281babf02eac778cdd4ac9
AyberkY <ayberkyaraneri@gmail.com>
2019-04-02 15:41:30 -0500

    .

--- Test/rangeTest_ground.py
+++ Test/rangeTest_ground.py
@@ -17,7 +17,7 @@ try:
     while True:
         data, rssi = Telem.receive()
 
-        if(type(data) != None):
+        if(data != None):
             print(str(data, 'ascii'))
             print('RSSI: ' + str(data[1]))
 
