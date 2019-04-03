03ac5ed8cb8fb67430ed83471de0b4aafbf5091f
davidcrobbins <47231078+davidcrobbins@users.noreply.github.com>
2019-04-03 15:48:59 -0500

    .

--- Camera/camera.py
+++ Camera/camera.py
@@ -1,35 +1,38 @@
 from picamera import PiCamera
 from time import sleep
+import sys
 
-cam = PiCamera()  
+sys.path.insert(0, '/home/pi/ISS-SpaceGrant18-19/IMU/MPU9250')
+import mpu9250
+
+IMU1 = mpu9250.MPU9250()
+cam = PiCamera()
 
 def accelMEASURE():                 #defines a function that takes Ayush function and makes it check if the camera should happen
-    currentACCEL = ayushFUNCTION(-1)
+    accel_DICT = IMU1.readAccel()
+    x_ACCEL, y_ACCEL, z_ACCEL = accel_DICT['x'], accel_DICT['y'], accel_DICT['z']
+
+    currentACCEL = (x_ACCEL**2 + y_ACCEL**2 + z_ACCEL**2)**0.5
     pastACCEL = ayushFUNCTION(-10)
-    
+
     avgACCEL = (abs(currentACCEL) + abs(pastACCEL))/2
+    pastACCEL = currentACCEL
     return avgACCEL
 
 
 def cameraRECORD():                      #function that does the camera
     flightSTATUS = 1                     #flight is happening
     accelCHECK = accelMEASURE()
-    
+
     cam.start_recording('path...')      #starts recording
-    
+
     while flightSTATUS == 1:            #check if rocket is in flight
         sleep(10)
         if accelCHECK <= 10**-2:
             flightSTATUS -= 1
         else:
             continue
-        
+
     cam.stop_recording()                #stops recording
 
 cameraRECORD()
-
-
-    
-
-
-
