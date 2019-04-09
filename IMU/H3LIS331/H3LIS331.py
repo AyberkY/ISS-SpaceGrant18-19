5ff4301444ca5d0451991184f764ef0566ffacca
Misapo <47199617+Misapo@users.noreply.github.com>
2019-04-08 20:00:51 -0500

    ojibosgkwefnjb

--- IMU/H3LIS331/H3LIS331.py
+++ IMU/H3LIS331/H3LIS331.py
@@ -105,17 +105,17 @@ class H3LIS331():
 
 		return {'x' : xAccl, 'y' : yAccl, 'z' : zAccl}
 
-    ## Data Convert
-    # @param [in] self The object pointer.
-    # @param [in] data1 LSB
-    # @param [in] data2 MSB
-    # @retval Value MSB+LSB(int 16bit)
-    def dataConv(self, data1, data2):
-        value = data1 | (data2 << 8)
+	    ## Data Convert
+	    # @param [in] self The object pointer.
+	    # @param [in] data1 LSB
+	    # @param [in] data2 MSB
+	    # @retval Value MSB+LSB(int 16bit)
+	    def dataConv(self, data1, data2):
+	        value = data1 | (data2 << 8)
 
-        if(value & (1 << 16 - 1)):
-            value -= (1<<16)
-        return value
+	        if(value & (1 << 16 - 1)):
+	            value -= (1<<16)
+	        return value
 
 
 """
