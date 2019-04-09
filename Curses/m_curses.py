ec3d7f949ed37479a7cb50dc74d2566e5ce3b264
mayachatt <44557298+mayachatt@users.noreply.github.com>
2019-04-08 20:22:34 -0500

    count

--- Curses/m_curses.py
+++ Curses/m_curses.py
@@ -3,10 +3,10 @@ import sys
 import curses
 
 def testGUI(stdscr):
-    for i in range(100):
+    for i in range(10):
         while True:
             stdscr.erase()
-            stdscr.addstr(0,10,"more sensors",curses.color_pair(color))
+            stdscr.addstr(0,10,"more sensors" + str(i))
             stdscr.refresh()
 
             time.sleep(0.1)
