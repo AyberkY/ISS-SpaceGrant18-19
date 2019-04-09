e61eb3a52f68c2c9d22db8a6d27bd0bcdccd59d5
godsell3 <44557485+godsell3@users.noreply.github.com>
2019-04-08 19:11:37 -0500

    curses work

---
+++ CursesTest/CursesTest.py
@@ -0,0 +1,21 @@
+import curses
+
+stdscr = curses.initscr()
+#stdscr is a window object representing the entire screen
+
+curses.noecho()
+#turns off automatic echoing of keys to the screen
+
+curses.cbreak()
+#react to keys instantly (without pressing enter)
+
+stdscr.keypad(1)
+#makes curses expect cursor keys, navigation keys (Page Up, Home)
+
+#reverse the above w/ nocbreak, echo, keypad(0)
+
+for i in range(100):
+    try:
+        stdscr.addstr(0,0,"Count: " + i)
+    except curses.error:
+        pass
---
+++ GUI/GUI.py
@@ -0,0 +1,4 @@
+import curses
+
+stdscr = curses.initscr()
+#stdscr is a window object representing the entire screen
