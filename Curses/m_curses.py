e68efc6bf82589cc6143ce8d530969d6a0170d51
mayachatt <44557298+mayachatt@users.noreply.github.com>
2019-04-08 20:19:35 -0500

    no colors

--- Curses/m_curses.py
+++ Curses/m_curses.py
@@ -3,19 +3,11 @@ import sys
 import curses
 
 def testGUI(stdscr):
-
-    curses.init_pair(1, curses.COLOR_RED, curses.COLOR_YELLOW)
-
-    color = 1
-
     for i in range(100):
         while True:
             stdscr.erase()
             stdscr.addstr(0,10,"more sensors",curses.color_pair(color))
             stdscr.refresh()
-            color += 1
-            if color == 3:
-                color = 1
 
             time.sleep(0.1)
 
