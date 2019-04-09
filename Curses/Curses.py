f3b0a5dd7329ce1b8ef53f2be8a20f55a9a9a1f3
godsell3 <44557485+godsell3@users.noreply.github.com>
2019-04-08 20:02:39 -0500

    colors!

--- Curses/Curses.py
+++ Curses/Curses.py
@@ -3,13 +3,18 @@ import time
 
 def a_curses(stdscr):
 
+    curses.init_pair(1,curses.COLOR_RED,curses.COLOR_GREEN)
+
     stdscr.addstr(0,0,"Count: ",curses.A_BOLD)
 
     for i in range(100):
         try:
             stdscr.refresh()
             stdscr.addstr(0,10,str(i))
-            time.sleep(1)
+            time.sleep(0.25)
+
+            if i == 20:
+                stdscr.addstr(2,2,"woot woot",curses.A_BLINK,curses.color_pair(1))
         except:
             print("there was an error")
             break
