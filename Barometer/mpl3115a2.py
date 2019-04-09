53d125b4c7a56f4a983b05b3711cb120bce1ce62
AyberkY <ayberkyaraneri@gmail.com>
2019-04-08 19:45:45 -0500

    .

--- Barometer/mpl3115a2.py
+++ Barometer/mpl3115a2.py
@@ -5,26 +5,26 @@ import time
 class Barometer:
     def __init__(self):
         # Get I2C bus
-        bus = smbus2.SMBus(1)
+        self.bus = smbus2.SMBus(1)
 
         # MPL3115A2 address, 0x60(96)
         # Select control register, 0x26(38)
         #		0xB9(185)	Active mode, OSR = 128, Altimeter mode
-        bus.write_byte_data(0x60, 0x26, 0xB9)
+        self.bus.write_byte_data(0x60, 0x26, 0xB9)
         # MPL3115A2 address, 0x60(96)
         # Select data configuration register, 0x13(19)
         #		0x07(07)	Data ready event enabled for altitude, pressure, temperature
-        bus.write_byte_data(0x60, 0x13, 0x07)
+        self.bus.write_byte_data(0x60, 0x13, 0x07)
         # MPL3115A2 address, 0x60(96)
         # Select control register, 0x26(38)
         #		0xB9(185)	Active mode, OSR = 128, Altimeter mode
-        bus.write_byte_data(0x60, 0x26, 0xB9)
+        self.bus.write_byte_data(0x60, 0x26, 0xB9)
 
     def getData(self):
         # MPL3115A2 address, 0x60(96)
         # Read data back from 0x00(00), 6 bytes
         # status, tHeight MSB1, tHeight MSB, tHeight LSB, temp MSB, temp LSB
-        data = bus.read_i2c_block_data(0x60, 0x00, 6)
+        data = self.bus.read_i2c_block_data(0x60, 0x00, 6)
 
         # Convert the data to 20-bits
         tHeight = ((data[1] * 65536) + (data[2] * 256) + (data[3] & 0xF0)) / 16
@@ -39,7 +39,7 @@ class Barometer:
         # MPL3115A2 address, 0x60(96)
         # Read data back from 0x00(00), 4 bytes
         # status, pres MSB1, pres MSB, pres LSB
-        data = bus.read_i2c_block_data(0x60, 0x00, 4)
+        data = self.bus.read_i2c_block_data(0x60, 0x00, 4)
 
         # Convert the data to 20-bits
         pres = ((data[1] * 65536) + (data[2] * 256) + (data[3] & 0xF0)) / 16
