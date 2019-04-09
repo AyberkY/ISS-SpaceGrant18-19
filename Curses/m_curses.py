e5182bb9804973b21dd18616cb6e6fb10665bd2f
mayachatt <44557298+mayachatt@users.noreply.github.com>
2019-04-08 20:26:48 -0500

    yup

--- Curses/m_curses.py
+++ Curses/m_curses.py
@@ -6,7 +6,7 @@ def testGUI(stdscr):
     for i in range(10):
         while True:
             stdscr.erase()
-            stdscr.addstr(0,10,"more sensors" + str(i))
+            stdscr.addstr(0,0,str(i))
             stdscr.refresh()
 
             time.sleep(0.1)
