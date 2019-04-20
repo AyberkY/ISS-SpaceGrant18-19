e9ea72db1a7966de6f1834e2e324271bce7be8e0
mayachatt <44557298+mayachatt@users.noreply.github.com>
2019-04-20 00:12:47 -0500

    add system check column

--- GUI/GUI.py
+++ GUI/GUI.py
@@ -7,63 +7,71 @@ def cursesTest(stdscr):
     curses.init_pair(1,curses.COLOR_BLACK,curses.COLOR_CYAN)
     #second color is background color
 
-    stdscr.addstr(0,30,"Hour: ",curses.A_BOLD)
-    stdscr.addstr(1,30,"Minutes: ",curses.A_BOLD)
-    stdscr.addstr(2,30,"Seconds: ",curses.A_BOLD)
-    stdscr.addstr(3,30,"Millisec: ",curses.A_BOLD)
+    stdscr.addstr(0,0,"GPS: ",curses.A_BOLD)
+    stdscr.addstr(1,0,"ADC: ",curses.A_BOLD)
+    stdscr.addstr(2,0,"Barometer: ",curses.A_BOLD)
+    stdscr.addstr(3,0,"IMU: ",curses.A_BOLD)
+    stdscr.addstr(4,0,"Telemetry: ",curses.A_BOLD)
+    stdscr.addstr(5,0,"Camera: ",curses.A_BOLD)
 
-    stdscr.addstr(5,30,"Latitude: ",curses.A_BOLD)
-    stdscr.addstr(6,30,"Longitude: ",curses.A_BOLD)
-    stdscr.addstr(7,30,"Altitude: ",curses.A_BOLD)
-    stdscr.addstr(8,30,"Satellites: ",curses.A_BOLD)
 
-    stdscr.addstr(10,30,"read_ADC(0): ",curses.A_BOLD)
-    stdscr.addstr(11,30,"read_ADC(1): ",curses.A_BOLD)
-    stdscr.addstr(12,30,"read_ADC(2): ",curses.A_BOLD)
+    stdscr.addstr(0,30,"Hour: ")
+    stdscr.addstr(1,30,"Minutes: ")
+    stdscr.addstr(2,30,"Seconds: ")
+    stdscr.addstr(3,30,"Millisec: ")
 
-    stdscr.addstr(0,60,"baro_pressure: ",curses.A_BOLD)
-    stdscr.addstr(1,60,"baro_altitude: ",curses.A_BOLD)
-    stdscr.addstr(2,60,"Temp (C): ",curses.A_BOLD)
+    stdscr.addstr(5,30,"Latitude: ")
+    stdscr.addstr(6,30,"Longitude: ")
+    stdscr.addstr(7,30,"Altitude: ")
+    stdscr.addstr(8,30,"Satellites: ")
 
-    stdscr.addstr(4,60,"Accel x: ",curses.A_BOLD)
-    stdscr.addstr(5,60,"Accel y: ",curses.A_BOLD)
-    stdscr.addstr(6,60,"Accel z: ",curses.A_BOLD)
-    stdscr.addstr(7,60,"Gyro x: ",curses.A_BOLD)
-    stdscr.addstr(8,60,"Gyro y: ",curses.A_BOLD)
-    stdscr.addstr(9,60,"Gyro z: ",curses.A_BOLD)
+    stdscr.addstr(10,30,"read_ADC(0): ")
+    stdscr.addstr(11,30,"read_ADC(1): ")
+    stdscr.addstr(12,30,"read_ADC(2): ")
+
+
+    stdscr.addstr(0,60,"baro_pressure: ")
+    stdscr.addstr(1,60,"baro_altitude: ")
+    stdscr.addstr(2,60,"Temp (C): ")
+
+    stdscr.addstr(4,60,"Accel x: ")
+    stdscr.addstr(5,60,"Accel y: ")
+    stdscr.addstr(6,60,"Accel z: ")
+    stdscr.addstr(7,60,"Gyro x: ")
+    stdscr.addstr(8,60,"Gyro y: ")
+    stdscr.addstr(9,60,"Gyro z: ")
 
     while True:
-        try:
-            stdscr.refresh()
-            stdscr.addstr(0,45,str(random.randint(0,100))+"   ")
-            stdscr.addstr(1,45,str(random.randint(0,100))+"   ")
-            stdscr.addstr(2,45,str(random.randint(0,100))+"   ")
-            stdscr.addstr(3,45,str(random.randint(0,100))+"   ")
+        stdscr.refresh()
 
-            stdscr.addstr(5,45,str(random.randint(0,100))+"   ")
-            stdscr.addstr(6,45,str(random.randint(0,100))+"   ")
-            stdscr.addstr(7,45,str(random.randint(0,100))+"   ")
-            stdscr.addstr(8,45,str(random.randint(0,100))+"   ")
+        stdscr.addstr(0,11,"GO",curses.A_BOLD)
 
-            stdscr.addstr(10,45,str(random.randint(0,100))+"   ")
-            stdscr.addstr(11,45,str(random.randint(0,100))+"   ")
-            stdscr.addstr(12,45,str(random.randint(0,100))+"   ")
+        stdscr.addstr(0,45,str(random.randint(0,100))+"   ")
+        stdscr.addstr(1,45,str(random.randint(0,100))+"   ")
+        stdscr.addstr(2,45,str(random.randint(0,100))+"   ")
+        stdscr.addstr(3,45,str(random.randint(0,100))+"   ")
 
-            stdscr.addstr(0,75,str(random.randint(0,100))+"   ")
-            stdscr.addstr(1,75,str(random.randint(0,100))+"   ")
-            stdscr.addstr(2,75,str(random.randint(0,100))+"   ")
+        stdscr.addstr(5,45,str(random.randint(0,100))+"   ")
+        stdscr.addstr(6,45,str(random.randint(0,100))+"   ")
+        stdscr.addstr(7,45,str(random.randint(0,100))+"   ")
+        stdscr.addstr(8,45,str(random.randint(0,100))+"   ")
 
-            stdscr.addstr(4,75,str(random.randint(0,100))+"   ")
-            stdscr.addstr(5,75,str(random.randint(0,100))+"   ")
-            stdscr.addstr(6,75,str(random.randint(0,100))+"   ")
-            stdscr.addstr(7,75,str(random.randint(0,100))+"   ")
-            stdscr.addstr(8,75,str(random.randint(0,100))+"   ")
-            stdscr.addstr(9,75,str(random.randint(0,100))+"   ")
+        stdscr.addstr(10,45,str(random.randint(0,100))+"   ")
+        stdscr.addstr(11,45,str(random.randint(0,100))+"   ")
+        stdscr.addstr(12,45,str(random.randint(0,100))+"   ")
 
-            time.sleep(0.5)
 
-        except:
-            print("there was an error")
-            break
+        stdscr.addstr(0,75,str(random.randint(0,100))+"   ")
+        stdscr.addstr(1,75,str(random.randint(0,100))+"   ")
+        stdscr.addstr(2,75,str(random.randint(0,100))+"   ")
+
+        stdscr.addstr(4,75,str(random.randint(0,100))+"   ")
+        stdscr.addstr(5,75,str(random.randint(0,100))+"   ")
+        stdscr.addstr(6,75,str(random.randint(0,100))+"   ")
+        stdscr.addstr(7,75,str(random.randint(0,100))+"   ")
+        stdscr.addstr(8,75,str(random.randint(0,100))+"   ")
+        stdscr.addstr(9,75,str(random.randint(0,100))+"   ")
+
+        time.sleep(0.5)
 
 curses.wrapper(cursesTest)
