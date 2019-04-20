89668cb2cd55df28ef3a6822d254db0ce46b79d6
mayachatt <44557298+mayachatt@users.noreply.github.com>
2019-04-19 23:53:33 -0500

    moving existing columns

--- GUI/GUI.py
+++ GUI/GUI.py
@@ -7,30 +7,30 @@ def cursesTest(stdscr):
     curses.init_pair(1,curses.COLOR_BLACK,curses.COLOR_CYAN)
     #second color is background color
 
-    stdscr.addstr(0,0,"Hour: ",curses.A_BOLD)
-    stdscr.addstr(1,0,"Minutes: ",curses.A_BOLD)
-    stdscr.addstr(2,0,"Seconds: ",curses.A_BOLD)
-    stdscr.addstr(3,0,"Millisec: ",curses.A_BOLD)
+    stdscr.addstr(0,30,"Hour: ",curses.A_BOLD)
+    stdscr.addstr(1,30,"Minutes: ",curses.A_BOLD)
+    stdscr.addstr(2,30,"Seconds: ",curses.A_BOLD)
+    stdscr.addstr(3,30,"Millisec: ",curses.A_BOLD)
 
-    stdscr.addstr(5,0,"Latitude: ",curses.A_BOLD)
-    stdscr.addstr(6,0,"Longitude: ",curses.A_BOLD)
-    stdscr.addstr(7,0,"Altitude: ",curses.A_BOLD)
-    stdscr.addstr(8,0,"Satellites: ",curses.A_BOLD)
+    stdscr.addstr(5,30,"Latitude: ",curses.A_BOLD)
+    stdscr.addstr(6,30,"Longitude: ",curses.A_BOLD)
+    stdscr.addstr(7,30,"Altitude: ",curses.A_BOLD)
+    stdscr.addstr(8,30,"Satellites: ",curses.A_BOLD)
 
-    stdscr.addstr(10,0,"read_ADC(0): ",curses.A_BOLD)
-    stdscr.addstr(11,0,"read_ADC(1): ",curses.A_BOLD)
-    stdscr.addstr(12,0,"read_ADC(2): ",curses.A_BOLD)
+    stdscr.addstr(10,30,"read_ADC(0): ",curses.A_BOLD)
+    stdscr.addstr(11,30,"read_ADC(1): ",curses.A_BOLD)
+    stdscr.addstr(12,30,"read_ADC(2): ",curses.A_BOLD)
 
-    stdscr.addstr(0,30,"baro_pressure: ",curses.A_BOLD)
-    stdscr.addstr(1,30,"baro_altitude: ",curses.A_BOLD)
-    stdscr.addstr(2,30,"Temp (C): ",curses.A_BOLD)
+    stdscr.addstr(0,60,"baro_pressure: ",curses.A_BOLD)
+    stdscr.addstr(1,60,"baro_altitude: ",curses.A_BOLD)
+    stdscr.addstr(2,60,"Temp (C): ",curses.A_BOLD)
 
-    stdscr.addstr(4,30,"Accel x: ",curses.A_BOLD)
-    stdscr.addstr(5,30,"Accel y: ",curses.A_BOLD)
-    stdscr.addstr(6,30,"Accel z: ",curses.A_BOLD)
-    stdscr.addstr(7,30,"Gyro x: ",curses.A_BOLD)
-    stdscr.addstr(8,30,"Gyro y: ",curses.A_BOLD)
-    stdscr.addstr(9,30,"Gyro z: ",curses.A_BOLD)
+    stdscr.addstr(4,60,"Accel x: ",curses.A_BOLD)
+    stdscr.addstr(5,60,"Accel y: ",curses.A_BOLD)
+    stdscr.addstr(6,60,"Accel z: ",curses.A_BOLD)
+    stdscr.addstr(7,60,"Gyro x: ",curses.A_BOLD)
+    stdscr.addstr(8,60,"Gyro y: ",curses.A_BOLD)
+    stdscr.addstr(9,60,"Gyro z: ",curses.A_BOLD)
 
     while True:
         try:
