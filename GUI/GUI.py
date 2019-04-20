39439fcecb1648449de7315309f5beef1a036194
mayachatt <44557298+mayachatt@users.noreply.github.com>
2019-04-20 00:48:08 -0500

    color text with black background

--- GUI/GUI.py
+++ GUI/GUI.py
@@ -4,8 +4,8 @@ import random
 
 def cursesTest(stdscr):
 
-    curses.init_pair(1,curses.COLOR_GREEN,-1)
-    curses.init_pair(2,curses.COLOR_RED,-1)
+    curses.init_pair(1,curses.COLOR_GREEN,curses.COLOR_BLACK)
+    curses.init_pair(2,curses.COLOR_RED,curses.COLOR_BLACK)
 
     stdscr.addstr(0,0,"GPS: ",curses.A_BOLD)
     stdscr.addstr(1,0,"ADC: ",curses.A_BOLD)
