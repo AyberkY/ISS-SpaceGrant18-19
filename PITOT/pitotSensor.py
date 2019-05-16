02680af4325c37f5ddbb01170d1533a369e7307e
Ayberk Yaraneri <35669067+AyberkY@users.noreply.github.com>
2019-05-16 03:59:58 -0500

    .

--- PITOT/pitotSensor.py
+++ PITOT/pitotSensor.py
@@ -6,15 +6,15 @@ class PITOT:
     def __init__(self):
         self.address = 0x28
         self.offset = 0
-
-        sum = 0
-        for i in range(100):
-            data = bus.read_i2c_block_data(self.address, 0, 2)
-            pressure_raw = data[0] << 8
-            pressure_raw += data[1]
-            sum += pressure_raw
-
-        self.offset = sum / 100
+        #
+        # sum = 0
+        # for i in range(100):
+        #     data = bus.read_i2c_block_data(self.address, 0, 2)
+        #     pressure_raw = data[0] << 8
+        #     pressure_raw += data[1]
+        #     sum += pressure_raw
+        #
+        # self.offset = sum / 100
 
     def getPressure(self):
         data = bus.read_i2c_block_data(self.address, 0, 2)
