3009ce13cbb59398aefe0223eae4a2c6cec218b4
Ayberk Yaraneri <35669067+AyberkY@users.noreply.github.com>
2019-05-16 03:58:22 -0500

    .

--- PITOT/pitotSensor.py
+++ PITOT/pitotSensor.py
@@ -4,18 +4,21 @@ bus = smbus.SMBus(1)
 
 class PITOT:
     def __init__(self):
-        self.slaveAddress = 0x28
+        self.address = 0x28
         self.offset = 0
 
+        sum = 0
+        for i in range(100):
+            data = bus.read_i2c_block_data(self.address, 0, 2)
+            pressure_raw = data[0] << 8
+            pressure_raw += data[1]
+            sum += pressure_raw
+
+        self.offset = sum / 100
+
     def getPressure(self):
-        data = bus.read_i2c_block_data(0x28, 0, 2)
+        data = bus.read_i2c_block_data(self.address, 0, 2)
         pressure_raw = data[0] << 8
         pressure_raw += data[1]
 
         return pressure_raw - self.offset
-
-    def calPressure(self):
-        sum = 0
-        for i in range(100):
-            sum += self.getPressure()
-        self.offset = sum / 100
