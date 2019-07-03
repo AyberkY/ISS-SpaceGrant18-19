879a9656f24fed8b31cb2cf5aaed0dadde36ffcc
AyberkY <ayberkyaraneri@gmail.com>
2019-07-03 18:49:50 -0500

    .

--- IMU/H3LIS331/H3LIS331.py
+++ IMU/H3LIS331/H3LIS331.py
@@ -14,7 +14,7 @@ import time
 bus = smbus.SMBus(1)
 
 # I2C address of the device
-H3LIS331DL_DEFAULT_ADDRESS			= 0x0C
+H3LIS331DL_DEFAULT_ADDRESS			= 0x12
 
 # H3LIS331DL Register Map
 H3LIS331DL_REG_WHOAMI					= 0x0F # Who Am I Register
