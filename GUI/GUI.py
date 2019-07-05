40ff49cfe951c41d87fae77b5ebc1fabd42ebe29
mayachatt <44557298+mayachatt@users.noreply.github.com>
2019-07-05 17:36:07 -0500

    gui

--- GUI/GUI.py
+++ GUI/GUI.py
@@ -18,7 +18,8 @@ def cursesTest(stdscr):
 
     stdscr = curses.initscr()
     stdscr.clear()
-    win = stdscr.newwin(25,100,10,10)
+#    win = stdscr.newwin(25,100,10,10)
+    win = stdscr.subwin(start_x,start_y,scr_height,scr_width)
 
     curses.init_pair(1,curses.COLOR_GREEN,curses.COLOR_BLACK)
     curses.init_pair(2,curses.COLOR_RED,curses.COLOR_BLACK)
