b71ee6050696f2ed0257aaa4ed639f086e833d34
mayachatt <44557298+mayachatt@users.noreply.github.com>
2019-07-05 17:13:13 -0500

    gui

--- GUI/GUI.py
+++ GUI/GUI.py
@@ -11,7 +11,8 @@ def cursesTest(stdscr):
 
     TELEM1 = RFM9X.RFM9X()
 
-    curses.newwin(25,100,10,10)
+    stdscr = curses.initscr()
+    win = curses.newwin(25,100,10,10)
 
     curses.init_pair(1,curses.COLOR_GREEN,curses.COLOR_BLACK)
     curses.init_pair(2,curses.COLOR_RED,curses.COLOR_BLACK)
