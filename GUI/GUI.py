30be237e5dd1b9838b68da73ae308f30583b93f4
AyberkY <ayberkyaraneri@gmail.com>
2019-07-05 21:24:06 -0500

    .

--- GUI/GUI.py
+++ GUI/GUI.py
@@ -7,10 +7,13 @@ sys.path.insert(0, '/home/pi/ISS-SpaceGrant18-19/Telemetry')
 
 import RFM9X
 
+
+
 def cursesTest(stdscr):
 
     TELEM1 = RFM9X.RFM9X()
 
+    [data, rssi] = TELEM1.receive()
 
     curses.init_pair(1,curses.COLOR_GREEN,curses.COLOR_BLACK)
     curses.init_pair(2,curses.COLOR_RED,curses.COLOR_BLACK)
