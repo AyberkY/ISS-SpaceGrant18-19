33367a18919367965247e9303f77384d26d2a0fa
AyberkY <ayberkyaraneri@gmail.com>
2019-08-08 00:46:52 -0500

    rssi

--- GUI/GUI.py
+++ GUI/GUI.py
@@ -21,24 +21,26 @@ def cursesTest(stdscr):
     curses.init_pair(4,curses.COLOR_CYAN,curses.COLOR_BLACK)
 
     #Column 2
-    stdscr.addstr(0,0,"Timestamp: ")
+    stdscr.addstr(0,0,"Timestamp: ",curses.A_BOLD)
 
-    stdscr.addstr(2,0,"MAX_ACCEL: ")
-    stdscr.addstr(3,0,"BOOST_DUR: ")
-    stdscr.addstr(4,0,"MAX_VELO: ")
-    stdscr.addstr(5,0,"COAST_DUR: ")
-    stdscr.addstr(6,0,"MAX_ALT: ")
+    stdscr.addstr(2,0,"MAX_ACCEL: ",curses.A_BOLD)
+    stdscr.addstr(3,0,"BOOST_DUR: ",curses.A_BOLD)
+    stdscr.addstr(4,0,"MAX_VELO: ",curses.A_BOLD)
+    stdscr.addstr(5,0,"COAST_DUR: ",curses.A_BOLD)
+    stdscr.addstr(6,0,"MAX_ALT: ",curses.A_BOLD)
 
-    stdscr.addstr(8,0,"DROGUE_DESCENT_VEL: ")
-    stdscr.addstr(9,0,"MAIN_DEPLOYMENT_ALT: ")
-    stdscr.addstr(10,0,"MAIN_DESCENT_VEL: ")
+    stdscr.addstr(8,0,"DROGUE_DESCENT_VEL: ",curses.A_BOLD)
+    stdscr.addstr(9,0,"MAIN_DEPLOYMENT_ALT: ",curses.A_BOLD)
+    stdscr.addstr(10,0,"MAIN_DESCENT_VEL: ",curses.A_BOLD)
     stdscr.addstr(11,0,"SUCCESSFULL_CHARGE: ",curses.A_BOLD)
 
-    stdscr.addstr(13,0,"Latitude: ")
-    stdscr.addstr(14,0,"Longitude: ")
-    stdscr.addstr(15,0,"Altitude: ")
+    stdscr.addstr(13,0,"Latitude: ",curses.A_BOLD)
+    stdscr.addstr(14,0,"Longitude: ",curses.A_BOLD)
+    stdscr.addstr(15,0,"Altitude: ",curses.A_BOLD)
 
-    stdscr.addstr(17,5,"MORE SENSORS.",curses.color_pair(4) | curses.A_BOLD)
+    stdscr.addstr(17,0,"RSSI: ",curses.A_BOLD)
+
+    stdscr.addstr(19,5,"MORE SENSORS.",curses.color_pair(4) | curses.A_BOLD)
 
     while True:
         try:
@@ -62,20 +64,22 @@ def cursesTest(stdscr):
                 stdscr.addstr(5,15,str(data[3])+"   ")
                 stdscr.addstr(6,15,str(data[4])+"   ")
 
-                stdscr.addstr(8,15,str(data[6])+"   ")
-                stdscr.addstr(9,15,str(data[7])+"   ")
-                stdscr.addstr(10,15,str(data[8])+"   ")
+                stdscr.addstr(8,25,str(data[6])+"   ")
+                stdscr.addstr(9,25,str(data[7])+"   ")
+                stdscr.addstr(10,25,str(data[8])+"   ")
 
                 if data[5] == 1:
-                    stdscr.addstr(11,15,"PRIMARY")
+                    stdscr.addstr(11,25,"PRIMARY")
                 elif data[5] == 2:
-                    stdscr.addstr(11,15,"BACKUP")
+                    stdscr.addstr(11,25,"BACKUP")
                 else:
-                    stdscr.addstr(11,15,"NONE")
+                    stdscr.addstr(11,25,"NONE")
 
                 stdscr.addstr(13,15,str(data[9])+"   ")
                 stdscr.addstr(14,15,str(data[10])+"   ")
                 stdscr.addstr(15,15,str(data[11])+"   ")
+
+                stdscr.addstr(17,15,str(rssi)+"   ")
 
                 filehandle.write(str(data))
 
